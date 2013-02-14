6cb62254dc6b18c0b074e3969afbcaf4d38e530f
Rolf Erik Lekang <relekang@gmail.com>
2013-02-14 21:01:10 +0100

    remove unused code

--- assignment1/no/ntnu/tdt4190/TTTServer.java
+++ assignment1/no/ntnu/tdt4190/TTTServer.java
@@ -4,10 +4,6 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface TTTServer extends Remote {
-    public static final int MARK_NOT_SET = 0;
-    public static final int MARK_SET = 0;
-    public static final int MARK_SET_WON = 0;
-
     public String connect(String playerName, char mark, TTTServer opponent) throws RemoteException;
     public void setMark(int x, int y) throws RemoteException;
     public void resetBoard() throws RemoteException;
